6d0e0eac25cdb2f3d6a24d15212aff36081892b6
Aditya Kishore <aditya@maprtech.com>
2012-12-28 14:23:24 -0800

    HBASE-6285 HBase master should log INFO message when it attempts to assign a region (cherry picked from commit 8ad506565744573f435b12fafcbc204e4ca287df)

--- src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
+++ src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
@@ -1725,7 +1725,7 @@ public class AssignmentManager extends ZooKeeperListener {
         return; // Should get reassigned later when RIT times out.
       }
       try {
-        LOG.debug("Assigning region " + state.getRegion().getRegionNameAsString() +
+        LOG.info("Assigning region " + state.getRegion().getRegionNameAsString() +
           " to " + plan.getDestination().toString());
         long currentOfflineTimeStamp = state.getStamp();
         RegionOpeningState regionOpenState = serverManager.sendRegionOpen(plan.getDestination(),
