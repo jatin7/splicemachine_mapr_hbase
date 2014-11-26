d3adc6529ae3d21bfcb607551c0fca93802264b3
Jun Yuan <jyuan@splicemachine.com>
2014-11-25 16:21:42 -0800

    Revert "DB-1933 fix remove coprocessor does not update coprocessor list"
    This reverts commit 195f8be6b61edc66c53ff29273efa01aedbc20e1.

--- src/main/java/org/apache/hadoop/hbase/coprocessor/CoprocessorHost.java
+++ src/main/java/org/apache/hadoop/hbase/coprocessor/CoprocessorHost.java
@@ -227,7 +227,7 @@ public abstract class CoprocessorHost<E extends CoprocessorEnvironment> {
     updateCoprocessorList();
   }
 
-  protected synchronized void updateCoprocessorList()
+  protected void updateCoprocessorList()
   {
 	  coprocessorList.clear();
 	  for( E env: coprocessors){
@@ -767,7 +767,6 @@ public abstract class CoprocessorHost<E extends CoprocessorEnvironment> {
       LOG.error("Removing coprocessor '" + env.toString() + "' from " +
           "environment because it threw:  " + e,e);
       coprocessors.remove(env);
-      updateCoprocessorList();
       try {
         shutdown(env);
       } catch (Exception x) {
