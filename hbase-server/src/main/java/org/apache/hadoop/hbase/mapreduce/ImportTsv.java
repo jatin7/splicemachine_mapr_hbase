a1d4d58e2303c0cd9a14958d1ba8b3443ef434fd
Aditya Kishore <aditya@mapr.com>
2015-02-03 23:12:09 -0800

    MAPR-17043: Make PutCombiner configurable for M/R Jobs

--- hbase-server/src/main/java/org/apache/hadoop/hbase/mapreduce/ImportTsv.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/mapreduce/ImportTsv.java
@@ -513,7 +513,9 @@ public class ImportTsv extends Configured implements Tool {
         job.setReducerClass(TextSortReducer.class);
       } else {
         job.setMapOutputValueClass(Put.class);
-        job.setCombinerClass(PutCombiner.class);
+        if (job.getConfiguration().getBoolean(TableMapReduceUtil.ENABLE_PUTCOMBINER, false)) {
+          job.setCombinerClass(PutCombiner.class);
+        }
       }
       HFileOutputFormat.configureIncrementalLoad(job, table);
     } else {
